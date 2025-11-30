ac814ac62d5337193bdcf79b9f970847892322bf
Byran <BYRANTHC05.24@ichat.sp.edu.sg>
2025-11-30 23:32:52 +0800

    Lab2 v1.2

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     print(avg)
     min_max = find_min_max(num_list)
     print(min_max)
+    sort = sort_temp(num_list)
+    print(sort)
+    median = cal_median(num_list)
+    print(median)
 
 def display_main_menu():
     print("Enter some number seperated by commas")
@@ -28,7 +32,7 @@ def cal_averge(num_list):
     total = sum(num_list)
     avg = total / len(num_list)
 
-    return avg
+    return round(avg,1)
 
 def find_min_max(num_list):
     print("find min and max")
@@ -37,6 +41,28 @@ def find_min_max(num_list):
 
     return [minimum,maximum]
 
+def sort_temp(num_list):
+    print("sorted")
+    num_list = sorted(num_list) # small to big
+    #num_list = sorted(num_list,reverse=True)#big to small
+    return num_list
+
+def cal_median(num_list):
+    print("median")
+    
+    sort = sorted(num_list)
+    if len(num_list)%2 == 0: # even
+        middle_number_plus1 = len(num_list)//2
+        middle = middle_number_plus1 - 1
+        median = (sort[middle_number_plus1] + sort[middle])/2
+
+        return round(median,1)
+        
+        
+    
+    else: # odd
+        middle = len(sort) // 2
+        return sort[middle]
 
 if __name__ == "__main__":
     main()
